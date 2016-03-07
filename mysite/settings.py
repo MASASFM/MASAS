ae918a4e0b3fbdf0fa2e890ad594ee65a32f3956
jpic <jamespic@gmail.com>
2016-03-07 23:16:47 +0100

    fixup! Redact settings for OpenShift

--- mysite/settings.py
+++ mysite/settings.py
@@ -97,7 +97,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
