9ee687828fe8e4c00637365f37cbe65c826909c1
0648596375 <tbinetruy@gmail.com>
2016-03-15 02:01:30 +0100

    refactored API

--- frontend/routes/containers/Likes/Likes.jsx
+++ frontend/routes/containers/Likes/Likes.jsx
@@ -15,18 +15,19 @@ function mapStateToProps(state) {
 var getLikes = (dispatch, userPk) => {
 	$.ajax({
 		type: "GET",
-		url: 'api/users/' + userPk + '/',	
+		// url: 'api/users/' + userPk + '/',	
+		url: '/api/statuses/?users=' + userPk + '&status=1',	
 
 			 // -u"<client_id>:<client_secret>" 
 		success: (data) => {
 			
 			// get songs from the likes relationship
-			if(data.like_set.length > 0) {
-				var idString = data.like_set.map((like) => {return like.song.SC_ID}).join()
+			if(data.results.length > 0) {
+				var idString = data.results.map((like) => {return like.song.SC_ID}).join()
 				SC.get('tracks', {limit: 200, ids: idString}).then( (response) => {
 					console.log(response)
 					// this.setState({userInfo: data, userSCSongs: response})
-					dispatch({type: 'UPDATE_LIKES', SCinfo: response, userLikes: data.like_set})
+					dispatch({type: 'UPDATE_LIKES', SCinfo: response, userLikes: data.results})
 				})
 			}
 		},
