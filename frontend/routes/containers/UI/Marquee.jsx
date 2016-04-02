3b6f7df06e824f9c695792bdee3ff219d749949b
0648596375 <tbinetruy@gmail.com>
2016-04-02 18:47:24 +0200

    finished Marquee.css w/ responsive

--- frontend/routes/containers/UI/Marquee.jsx
+++ frontend/routes/containers/UI/Marquee.jsx
@@ -1,16 +1,6 @@
 var React = require("react")
 var ReactDOM = require("react-dom")
 
-// var {goToURL} = require("../../../MASAS_functions.jsx")
-// var {getCookie} = require("../../../MASAS_functions.jsx")
-// var { Link } = require("../../containers/UI/UI.jsx")
-
-// var Marquee = (props) => {
-// 	<div className="title MASAS_marquee">
-// 		<span>{ artworkPlaying.title }</span>
-// 	</div>
-// }
-
 let Marquee = React.createClass({
 	propTypes: {
 	},
@@ -18,25 +8,37 @@ let Marquee = React.createClass({
 	getInitialState: function() {
 		return {
 			overflow: false,			// (BOOL) is text overflowing
-			// refresh: false			// (BOOL) triggers a refresh necessary to recalc overflow
 		}
 	},
 
 	componentDidMount: function() {
+		// recompute animationDelay if need be on window resize
+		$(window).resize( () => {
+			this.checkTextOverflow()
+		})
+
+		// check if overflow and compute animationDelay if need be
 		this.checkTextOverflow()
 	},
 
-	componentDidUpdate: function(nextProps) {
+	// called after state change induced render
+	componentDidUpdate: function() {
 		this.checkTextOverflow()
 	},
 
+	componentWillReceiveProps: function(nextProps) {
+
+	},
+
+	// update state if overflow
 	checkTextOverflow: function() {
 		const element = this.refs.wrapper
 
 		if (element.offsetWidth < element.scrollWidth) {
 			console.log('overflow')
-			if(!this.state.overflow)
+			if(!this.state.overflow) {
 				this.setState({ overflow: true })
+			}
 		} else {
 			console.log('no overflow')
 			if(this.state.overflow)
@@ -45,10 +47,18 @@ let Marquee = React.createClass({
 	},
 
 	render: function() {
-		console.log(this.props.children)
+		// compute animationDelay on render
+		let animationDelay = null
+
+		if(typeof(this.refs.wrapper) !== "undefined") {
+			const a = this.refs.wrapper.offsetWidth
+			const b = this.refs.text.offsetWidth
+			animationDelay = -a / (a+b) * 10 + 's'
+		}
+
 		return (
 			<div className={ (this.props.className ? this.props.className : "") + " MASAS_marquee"} ref="wrapper">
-				<span className={ this.state.overflow ? "text" : "" }>{ this.props.children }</span>
+				<span ref="text" style={{ animationDelay: (animationDelay ? animationDelay : '0s') }} className={ this.state.overflow ? "text" : "" }>{ this.props.children }</span>
 			</div>
 		)
 	}
