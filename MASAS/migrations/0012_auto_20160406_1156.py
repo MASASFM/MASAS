a39db36e319a2eb3c095e723681bdd4ec0932214
jpic <jamespic@gmail.com>
2016-04-07 20:25:35 +0200

    Fix migration

--- MASAS/migrations/0012_auto_20160406_1156.py
+++ MASAS/migrations/0012_auto_20160406_1156.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('MASAS', '0012_auto_20160406_1156'),
+        ('MASAS', '0011_link_rewrite'),
     ]
 
     operations = [
