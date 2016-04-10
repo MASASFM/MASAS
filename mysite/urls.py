d6fe6b46500e44eea4a4979b527b64e867e225fd
jpic <jamespic@gmail.com>
2016-04-10 14:59:21 +0200

    Added StartSSL verification view

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,18 @@
+import os
+
 from django.conf.urls import url, include
+from django.views import generic
+from django import http
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 from rest_framework import routers
+
+
+class VerificationView(generic.View):
+    def get(self, request, *args, **kwargs):
+        return http.HttpResponse(os.environ.get('VERIFICATION_STRING'))
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -10,5 +20,6 @@ urlpatterns = [
     url(r'^auth/', include('rest_framework_social_oauth2.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^explorer/', include('explorer.urls')),
+    url(r'^masas.fm.html', VerificationView.as_view()),
     url(r'^', include('MASAS.urls')),
 ]
