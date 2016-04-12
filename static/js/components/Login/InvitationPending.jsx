c408ec7c19cde379dec00fc919856f65b58a13e8
0648596375 <tbinetruy@gmail.com>
2016-04-12 16:28:37 +0200

    met propType requirements on invitation pending disabled button

--- static/js/components/Login/InvitationPending.jsx
+++ static/js/components/Login/InvitationPending.jsx
@@ -22,7 +22,7 @@ var InvitationPending = React.createClass({
 				<div className="invitation-pending--wrapper">
 					<img src="/static/img/MASAS_icon_check_round.svg" alt="check logo" />
 					<h2>thank you for your interest</h2>
-					<Button isDisabled={ true } isBigButton={ true } isSecondaryAction={ true }>InvitationPending</Button>
+					<Button isDisabled={ true } isBigButton={ true } isSecondaryAction={ true } onClick={ () => {} }>InvitationPending</Button>
 					<p>
 						We will send you an email when your invitation will be approved by a MASAS lover. With MASAS, we
 						hope to nurture the true essence of an ever-expanding grassroots movement. Be part of the
