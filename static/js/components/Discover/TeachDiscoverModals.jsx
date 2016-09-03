8919f2d78c09a03b86544e342e77d02e19c52a0e
0648596375 <tbinetruy@gmail.com>
2016-09-03 16:11:06 +0200

    frontend update first teach ui modal

--- static/js/components/Discover/TeachDiscoverModals.jsx
+++ static/js/components/Discover/TeachDiscoverModals.jsx
@@ -4,7 +4,7 @@ var ReactDOM = require("react-dom")
 var ReactRedux = require("react-redux")
 var { mapStateToProps, mapDispatchToProps } = require("./containers/TeachDiscoverModals.jsx")
 
-// var {goToURL} = require("../../MASAS_functions.jsx")
+var { getCookie, updateProfileInfo } = require("../../MASAS_functions.jsx")
 var { Button } = require("../UI/UI.jsx")
 
 var TeachDiscoverModals = {}
@@ -19,13 +19,39 @@ TeachDiscoverModals.TeachDiscoverModal1 = ReactRedux.connect(
 		componentWillMount: function() {
 		},
 
+		updateUserStep: function() {
+			var header = "Bearer " + this.props.MASASuser
+
+			$.ajax({
+				type: 'POST',
+				url: '/api/usersteps/',
+				headers: {
+					"Authorization": header,
+				},
+				data: {
+					user: this.props.userData.url,
+					step: 5,
+				},
+				success: (r) => {
+					updateProfileInfo(this.props.toogleIsModalOpened)
+					console.log(r)
+				},
+				error: (e) => console.log(e),
+			})
+		},
+
 		render: function() {
 			return (
 				<div className="teach-modal--wrapper">
-					<p>
-						Discover music in other moods through the time of the day with the slider
+					<p className="bold">
+						Hey, Meet the Discovery Slider
 					</p>
-					<Button onClick={ () => {} }>Yeah!</Button>
+					<p>
+						It's your new friend! Match your daily journey with 6 different moods
+					</p>
+					<Button 
+						isBigButton={false}
+						onClick={ this.updateUserStep }>Yeah!</Button>
 
 				</div>
 			)
@@ -44,10 +70,34 @@ TeachDiscoverModals.TeachDiscoverModal2 = ReactRedux.connect(
 		componentWillMount: function() {
 		},
 
+		updateUserStep: function() {
+			var header = "Bearer " + this.props.MASASuser
+
+			$.ajax({
+				type: 'POST',
+				url: '/api/usersteps/',
+				headers: {
+					"Authorization": header,
+				},
+				data: {
+					user: this.props.userData.url,
+					step: 6,
+				},
+				success: (r) => {
+					updateProfileInfo(this.props.toogleIsModalOpened)
+					console.log(r)
+				},
+				error: (e) => console.log(e),
+			})
+		},
+
 		render: function() {
 			return (
 				<div className="teach-modal--wrapper">
 					BAR
+					<Button 
+						isBigButton={false}
+						onClick={ this.updateUserStep }>Got it!</Button>
 				</div>
 			)
 		}
