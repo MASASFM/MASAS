97b31a5755e4c5c11c412f69b867f95957accf1d
0648596375 <tbinetruy@gmail.com>
2016-09-14 00:11:20 +0200

    finished splashscreen mobile

--- static/js/bundle.js
+++ static/js/bundle.js
@@ -52507,8 +52507,43 @@ var SplashScreen = React.createClass({
 		this.hashtagSwiper = new Swiper('.hashtag-swiper-container', {
 			pagination: '.swiper-pagination',
 			paginationClickable: true,
-			autoplay: 2500,
-			autoplayDisableOnInteraction: false
+			autoplay: 6000,
+			autoplayDisableOnInteraction: false,
+			onSlideChangeStart: function onSlideChangeStart(instance) {
+				var urls = {
+					0: ["https://hd.unsplash.com/photo-1449168013943-3a15804bb41c"],
+					1: ["https://hd.unsplash.com/photo-1460500063983-994d4c27756c"],
+					2: ["https://hd.unsplash.com/38/L2NfDz5SOm7Gbf755qpw_DSCF0490.jpg"],
+					3: ["https://hd.unsplash.com/photo-1464022793855-9b1e5bd1d8d4"],
+					4: ["https://hd.unsplash.com/photo-1457256439474-fbd0369264a1"],
+					5: ["https://hd.unsplash.com/photo-1444080748397-f442aa95c3e5"]
+				};
+
+				var changeBackground = function changeBackground(hashtag) {
+					$('.splash-screen--wrapper').css('background-image', 'url(' + urls[hashtag][0] + ')');
+				};
+
+				switch (instance.activeIndex) {
+					case 0:
+						changeBackground(0);
+						break;
+					case 1:
+						changeBackground(1);
+						break;
+					case 2:
+						changeBackground(2);
+						break;
+					case 3:
+						changeBackground(3);
+						break;
+					case 4:
+						changeBackground(4);
+						break;
+					case 5:
+						changeBackground(5);
+						break;
+				}
+			}
 		});
 
 		this.mainSwiper = new Swiper('.main-swiper-container', {
@@ -52517,6 +52552,8 @@ var SplashScreen = React.createClass({
 			allowSwipeToNext: false,
 			onSlideChangeStart: function onSlideChangeStart(instance) {
 				_this.props.updateSplashScreenPage(instance.activeIndex);
+
+				if (instance.activeIndex !== 0) _this.hashtagSwiper.stopAutoplay();else _this.hashtagSwiper.startAutoplay();
 			}
 		});
 	},
--- static/js/components/App/SplashScreen.jsx
+++ static/js/components/App/SplashScreen.jsx
@@ -26,8 +26,43 @@ var SplashScreen = React.createClass({
 		this.hashtagSwiper = new Swiper('.hashtag-swiper-container', {
 			pagination: '.swiper-pagination',
 			paginationClickable: true,
-			autoplay: 2500,
+			autoplay: 6000,
 			autoplayDisableOnInteraction: false,
+			onSlideChangeStart: (instance) => {
+				const urls = {
+					0: ["https://hd.unsplash.com/photo-1449168013943-3a15804bb41c"],
+					1: ["https://hd.unsplash.com/photo-1460500063983-994d4c27756c"],
+					2: ["https://hd.unsplash.com/38/L2NfDz5SOm7Gbf755qpw_DSCF0490.jpg"],
+					3: ["https://hd.unsplash.com/photo-1464022793855-9b1e5bd1d8d4"],
+					4: ["https://hd.unsplash.com/photo-1457256439474-fbd0369264a1"],
+					5: ["https://hd.unsplash.com/photo-1444080748397-f442aa95c3e5"],
+				}
+
+				const changeBackground = (hashtag) => {
+					$('.splash-screen--wrapper').css('background-image', 'url(' + urls[hashtag][0] + ')')
+				}
+
+				switch(instance.activeIndex) {
+					case 0:
+						changeBackground(0)
+						break
+					case 1:
+						changeBackground(1)						
+						break
+					case 2:
+						changeBackground(2)
+						break
+					case 3:
+						changeBackground(3)
+						break
+					case 4:
+						changeBackground(4)
+						break
+					case 5:
+						changeBackground(5)
+						break
+				}
+			}
 		})
 
 		this.mainSwiper = new Swiper('.main-swiper-container', {
@@ -36,6 +71,11 @@ var SplashScreen = React.createClass({
 			allowSwipeToNext: false,
 			onSlideChangeStart: (instance) => {
 				this.props.updateSplashScreenPage(instance.activeIndex)
+
+				if(instance.activeIndex !== 0)
+					this.hashtagSwiper.stopAutoplay()
+				else
+					this.hashtagSwiper.startAutoplay()
 			}
 		})
 	},
