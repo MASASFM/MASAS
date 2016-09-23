c3a1531a426d506226643bce3a377503465f62ac
0648596375 <tbinetruy@gmail.com>
2016-09-23 17:44:29 +0100

    cleaned up code

--- static/js/components/Profile/CountryAutocomplete.jsx
+++ static/js/components/Profile/CountryAutocomplete.jsx
@@ -1,17 +1,9 @@
 var React = require("react")
-var ReactDOM = require("react-dom")
 
 var ReactRedux = require("react-redux")
 var { mapStateToProps, mapDispatchToProps } = require("./containers/CountryAutocomplete.jsx")
 
 var Autocomplete = require('react-autocomplete')
-
-// var {goToURL} = require("../../MASAS_functions.jsx")
-// var { Link } = require("../UI/UI.jsx")
-
-// var Template = (props) => {
-
-// }
 
 var CountryAutocomplete = React.createClass({
 	counter: 0,
@@ -47,7 +39,7 @@ var CountryAutocomplete = React.createClass({
 					cities: results.splice(0,15)
 				})
 			},
-			error: (e) => {
+			error: () => {
 			}
 		})
 	},
@@ -68,42 +60,6 @@ var CountryAutocomplete = React.createClass({
 	},
 
 	render: function() {
-		var styles = {
-			item: {
-				padding: '2px 6px',
-				cursor: 'default',
-				color: 'green'
-			},
-
-			highlightedItem: {
-				color: 'red',
-				background: 'hsl(200, 50%, 50%)',
-				padding: '2px 6px',
-				cursor: 'default'
-			},
-
-			menu: {
-				border: 'solid 1px #ccc'
-			},
-
-			menuStyle: {
-				borderRadius: '3px',
-				boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
-				background: 'rgba(255, 255, 255, 0.9)',
-				padding: '2px 0',
-				fontSize: '90%',
-				position: 'absolute',
-				overflow: 'auto',
-				maxHeight: 200 + 'px',
-				top: '100%',
-				left: 0,
-			},
-
-			wrapperStyle: {
-				display: 'flex'
-			}
-		}
-
 		return (
 			<div className="MASAS-textbox country-autocomplete--wrapper" style={{ position: 'relative' }}>
 				<label htmlFor="cities-autocomplete" className="MASAS-label">City</label>
@@ -113,7 +69,7 @@ var CountryAutocomplete = React.createClass({
 					items={ this.state.cities }
 					getItemValue={ (item) => item.name_ascii }
 					// menuStyle={ styles.menuStyle }
-					renderMenu={ (items, value, style) => {
+					renderMenu={ (items) => {
 						return <div className="menu-style">{ items }</div>
 					}}
 					inputProps={{ 
@@ -132,10 +88,10 @@ var CountryAutocomplete = React.createClass({
 					onChange={ this.onChange }
 					renderItem={ (item, isHighlighted ) => (
 						<div
-							  className={isHighlighted ? "highlighted-item" : 'item'}
-							  key={item.geoname_id}
-							  id={item.geoname_id}>
-							  {item.display_name}
+							className={isHighlighted ? "highlighted-item" : 'item'}
+							key={item.geoname_id}
+							id={item.geoname_id}>
+							{item.display_name}
 						</div>
 					)}
 				/>
