08ff7f97fb2e39660c0ffe753d84cfc2d55b1ff6
0648596375 <tbinetruy@gmail.com>
2016-09-28 15:07:51 +0200

    implemented required fields as props in UI.textbox

--- static/js/components/UI/Textbox.jsx
+++ static/js/components/UI/Textbox.jsx
@@ -12,11 +12,13 @@ var Textbox = React.createClass({
 		id: React.PropTypes.string, 				// name used to display textbox and error UI properly
 		onChange: React.PropTypes.func,			// callback called when input field changes
 		value: React.PropTypes.string,				// value of input field
+		required: React.PropTypes.bool, 			// is field required
 	},
 
 	getDefaultProps: function() {
 		return {
 			onChange: () => {},
+			required: false,
 		}
 	},
 
@@ -41,11 +43,15 @@ var Textbox = React.createClass({
 			<div className="MASAS-textbox">
 				<div className={"MASAS-textbox--wrapper" + (this.props.error ? " error" : "")}>
 					<input id={this.props.id} value={ this.props.value } onChange={ this.onInputChange } className="MASAS-text-input" type="text" />
-					<label className="MASAS-label" htmlFor={this.props.id}>
-						{ this.props.error ?
-							this.props.labelError 
-							:
-							this.props.children 
+					<label className={ "MASAS-label" + (this.props.isRequired ? " required" : "") } htmlFor={this.props.id}>
+						{ 
+							this.props.children ?
+								this.props.error ?
+									this.props.labelError 
+									:
+									( this.props.children + ( this.props.isRequired ? " *" : "" ) )
+								:
+									null
 						}
 					</label>
 				</div>
